768e7ce6bdcc9bc52c900a09ecc5a5a9a92eca66
Geme <90888249+GemeWolf@users.noreply.github.com>
2026-03-21 18:05:45 -0600

    feat: Integrate Cloudflare adapter and add Wrangler configuration for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,35 +2,41 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs.dadimarket.com', // 👈 Añade tu URL real aquí
-	integrations: [
-		starlight({
-			title: 'Dadi Market',
-			logo: {
-				src: './src/assets/DadiMarkerT.webp',
-			},
-			customCss: ['./src/styles/custom.css'],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/DadiMarket' }],
-			sidebar: [
-				{
-					label: 'Dadi Initial Rewards',
-					items: [
-						{ label: 'Introduction', slug: 'scripts/dadi-initial-rewards/introduction' },
-						{ label: 'Installation & SQL', slug: 'scripts/dadi-initial-rewards/installation' },
-						{ label: 'Configuration', slug: 'scripts/dadi-initial-rewards/configuration' },
-					],
-				},
-				{
-					label: 'Dadi Exotic Paints',
-					items: [
-						{ label: 'Introduction', slug: 'scripts/dadi-exotic-paints/introduction' },
-						{ label: 'Installation', slug: 'scripts/dadi-exotic-paints/installation' },
-						{ label: 'Configuration', slug: 'scripts/dadi-exotic-paints/configuration' },
-					],
-				},
-			],
-		}),
+  // 👈 Añade tu URL real aquí
+  site: 'https://docs.dadimarket.com',
+
+  integrations: [
+      starlight({
+          title: 'Dadi Market',
+          logo: {
+              src: './src/assets/DadiMarkerT.webp',
+          },
+          customCss: ['./src/styles/custom.css'],
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/DadiMarket' }],
+          sidebar: [
+              {
+                  label: 'Dadi Initial Rewards',
+                  items: [
+                      { label: 'Introduction', slug: 'scripts/dadi-initial-rewards/introduction' },
+                      { label: 'Installation & SQL', slug: 'scripts/dadi-initial-rewards/installation' },
+                      { label: 'Configuration', slug: 'scripts/dadi-initial-rewards/configuration' },
+                  ],
+              },
+              {
+                  label: 'Dadi Exotic Paints',
+                  items: [
+                      { label: 'Introduction', slug: 'scripts/dadi-exotic-paints/introduction' },
+                      { label: 'Installation', slug: 'scripts/dadi-exotic-paints/installation' },
+                      { label: 'Configuration', slug: 'scripts/dadi-exotic-paints/configuration' },
+                  ],
+              },
+          ],
+      }),
 	],
+
+  adapter: cloudflare(),
 });
